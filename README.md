1b1358cc77c58db20178a34ac4230ebba2ed0145
David Mitchell <david.c.mitchell@octanner.com>
2015-10-29 13:09:05 -0600

    initial setup based on ecugeo's snipit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# refactoring-practice
-Practice going through refactoring book
+# Refactoring Practice
+
+Practice going through refactoring book with ruby and just trying some of the things I've learned over the last couple days. Something of a personal improvement project to see if I can get some of the ideas behind DDD and Refactoring to stick.
+
+People are welcome to clone and run with this as much as they want.
+
---
+++ movie/test/movie_test.rb
@@ -0,0 +1,34 @@
+require_relative '../../test_helper'
+require_relative '../lib/movie'
+
+class BottlesTest < Minitest::Test
+
+  MOVIE_DATA = [
+    ["Jaws", Movie::REGULAR], 
+    ["Frozen", Movie::CHILDRENS], 
+    ["Spectre", Movie::NEW_RELEASE], 
+    ["Ghost Busters", Movie::REGULAR]
+  ]
+
+  def test_the_initial_movie_run
+    movies = MOVIE_DATA.map do |title, price_code|
+      Movie.new(title, price_code)
+    end
+
+    customer = Customer.new("George")
+
+    customer.add_rental(Rental.new(movies[0], 3))
+    customer.add_rental(Rental.new(movies[1], 2))
+    customer.add_rental(Rental.new(movies[2], 5))
+
+    expected = "Rental Record for George\n" +
+        "\tJaws\t3.5\n" +
+        "\tFrozen\t1.5\n" +
+        "\tSpectre\t15\n" +
+      "Amount owed is 20.0\n" +
+      "You earned 4 frequent renter points"
+
+    assert_equal expected, customer.statement
+  end
+
+end
