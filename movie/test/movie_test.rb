3203599a61587e4f5c735148bbdc29abf2153f68
David Mitchell <david.c.mitchell@octanner.com>
2015-10-29 16:09:12 -0600

    added html test

--- movie/test/movie_test.rb
+++ movie/test/movie_test.rb
@@ -1,7 +1,7 @@
 require_relative '../../test_helper'
 require_relative '../lib/movie'
 
-class BottlesTest < Minitest::Test
+class MovieTest < Minitest::Test
 
   MOVIE_DATA = [
     ["Jaws", Movie::REGULAR], 
@@ -24,17 +24,24 @@ class BottlesTest < Minitest::Test
 
   def test_output_rental_statement
     expected = "Rental Record for George\n" +
-        "\tJaws\t3.5\n" +
-        "\tFrozen\t1.5\n" +
-        "\tSpectre\t15\n" +
-      "Amount owed is 20.0\n" +
+      "  Jaws  3.5\n"+
+      "  Frozen  1.5\n"+
+      "  Spectre  15\n"+
+      "Amount owed is 20.0\n"+
       "You earned 4 frequent renter points"
 
     assert_equal expected, @customer.statement
   end
 
   def test_output_rental_statement_as_html
-
+    skip
+    expected = "<h1>Rentals for <em>George</em></h1><p>\n" +
+      "Jaws: 3.5<br>\n" +
+      "Frozen: 1.5<br>\n" +
+      "Spectre: 15<br>\n" + 
+      "<p>You owe <em>20.0</em><p>\n"
+      "On this rental you earned <em>4</em> frequent rental points<p>"
+    assert_equal expected, @customer.statement(HtmlStatement.new)
   end
 
 end
